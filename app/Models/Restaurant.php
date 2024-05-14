43f8d21de320e837578c9c6cb58e1d61e3729ca4
emanuele-pogliari <manu.pogliari@gmail.com>
2024-05-14 17:53:36 +0200

    restaurant_type relation created

--- app/Models/Restaurant.php
+++ app/Models/Restaurant.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Restaurant extends Model
 {
     use HasFactory;
+    public function types()
+    {
+        return $this->belongsToMany(Type::class);
+    }
 }
