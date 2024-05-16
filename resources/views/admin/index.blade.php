9e70395d6f6569b7c5425e26207954ec5d979729
emanuele-pogliari <manu.pogliari@gmail.com>
2024-05-16 11:01:42 +0200

    centered cards

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -3,17 +3,17 @@
 @section('content')
 
 <div class="container py-5">
-    <h1 class="text-center mb-4">Index</h1>
+    <h1 class="text-center mb-4">I tuoi Piatti</h1>
 
     <div class="row mb-5">
     @foreach ($dishes as $dish)
 
         <div class="col-3 card">
-            <img src="" class="card-img-top img-fluid" alt="">
-            <div class="card-body">
+            <img src="{{asset('storage/') . $dish->image}}" class="card-img-top img-fluid" alt="">
+            <div class="card-body text-center">
                 <h5 class="card-title">{{$dish->name}}</h5>
                 <p class="card-text">{{$dish->price}} €</p>
-                <a class="btn btn-info" href="{{route('admin.dishes.show', $dish->id)}}">Visualizza</a>
+                <a class="btn btn-primary" href="{{route('admin.dishes.show', $dish->id)}}">Visualizza</a>
             </div>
         </div>
     @endforeach
