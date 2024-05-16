9aad6366979205c6733a2ba5a8d4a19ba205185c
christiandigiandomenico <christian.digiandomenico1990@gmail.com>
2024-05-16 10:04:31 +0200

    Validation dishes done

--- app/Http/Requests/StoreDishRequest.php
+++ app/Http/Requests/StoreDishRequest.php
@@ -21,6 +21,34 @@ class StoreDishRequest extends FormRequest
      */
     public function rules(): array
     {
-        return [];
+        return [
+
+            'name' => 'required|max:255',
+            'image' => 'nullable|max:1000|mimes:jpg,bmp,png',
+            'description' => 'nullable|max:3000',
+            'ingredients' => 'required|max:255',
+            'price' => 'required|numeric|between:0,3000.99',
+
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'name.required' => "Il piatto deve avere un nome",
+            'name.max' => "Il piatto può avere massimo 255 caratteri",
+
+            'image.max' => "L'immagine può avere massimo 1000 caratteri",
+            'image.mimes' => "L'immagine deve essere un jpg,bmp o png",
+
+            'description.max' => 'La descrizione può essere di massimo 3000 caratteri',
+
+            'ingredients.required' => 'Il piatto deve avere degli ingredienti',
+            'ingredients.max' => 'Gli ingredienti possono avere massimo 255 caratteri',
+
+            'price.required' => 'Devi aggiungere un prezzo',
+            'price.numeric' => 'Il prezzo deve essere un numero',
+            'price.between' => 'Il prezzo deve essere compreso tra 0 e 3000.99',
+        ];
     }
 }
