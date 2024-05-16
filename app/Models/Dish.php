776a4bbbb8b221e59bc9ff1c5f2bceab2b4ee26c
christiansaccani <christian.saccani99@gmail.com>
2024-05-16 12:05:25 +0200

    soft delete added

--- app/Models/Dish.php
+++ app/Models/Dish.php
@@ -4,11 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Dish extends Model
 {
     use HasFactory;
-    protected $fillable = ['name', 'description', 'ingredients', 'price', 'visible', 'restaurant_id'];
+    use SoftDeletes;
+
+    protected $fillable = ['name', 'description', 'ingredients', 'price', 'visible', 'restaurant_id', 'deleted_at'];
     public function orders()
     {
         return $this->belongsToMany(Order::class);
