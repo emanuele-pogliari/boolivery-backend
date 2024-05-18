381fe52f008a53d1ac6a5190052d8f6be6f6d504
emanuele-pogliari <manu.pogliari@gmail.com>
2024-05-18 12:34:33 +0200

    add types json

--- app/Http/Controllers/Api/RestaurantController.php
+++ app/Http/Controllers/Api/RestaurantController.php
@@ -3,18 +3,23 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use App\Models\Dish;
 use App\Models\Restaurant;
+use App\Models\Type;
 use Illuminate\Http\Request;
 
 class RestaurantController extends Controller
 {
     public function index()
     {
-        $restaurants = Restaurant::with(['types', 'dishes'])->paginate(2);
+        $restaurants = Restaurant::with(['types', 'dishes'])->paginate(6);
         return response()->json([
             'success' => true,
             'restaurants' => $restaurants,
+        ]);
+        $types = Type::all();
+        return response()->json([
+            'success' => true,
+            'types' => $types,
         ]);
     }
 }
