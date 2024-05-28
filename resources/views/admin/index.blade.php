1a2bc08fafb477adf33fe84b20ca6ef22a706449
christiansaccani <christian.saccani99@gmail.com>
2024-05-28 19:00:29 +0200

    index page done

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -8,11 +8,11 @@
 
     <img class="restaurant-img" src="{{asset('storage/' . $restaurant->image)}}" alt="">
 
-    <div class="row mb-5">
+    <div class="row justify-content-center mb-5">
 
-        <div class="section-one-container">
+        <div class="section-one-container mb-5">
             
-            <div class="badge-container">
+            <div class="d-flex justify-content-center gap-2">
 
                 @foreach($restaurant->types as $type)
                         <p class="badge rounded-pill custom_badge mt-5">{{$type->type}}</p>
@@ -22,22 +22,27 @@
         </div>
     @endforeach
         
+{{-- ---------- --}}
 
+        <div class="alldishes_container">
+            @foreach ($dishes as $dish)
 
+                <div class="col-3 card card-container">
+                    <img src="{{ str_starts_with($dish->image, 'http') ? $dish->image : asset('storage/' . $dish->image)}}" class="card-img-top img-fluid" alt="">
 
-    @foreach ($dishes as $dish)
-
-        <div class="col-3 card card-container">
-            <img src="{{asset('storage/' . $dish->image)}}" class="card-img-top img-fluid" alt="">
-            <div class="card-body text-center">
-                <h5 class="card-title">{{$dish->name}}</h5>
-                <p class="card-text">{{$dish->price}} €</p>
-                <a class="show-button" href="{{route('admin.dishes.show', $dish->id)}}">Visualizza</a>
-            </div>
+                    <div class="card-body text-center">
+                        <h5 class="card-title">{{$dish->name}}</h5>
+                        <p class="card-text">{{$dish->price}} €</p>
+                        <a class="show-button" href="{{route('admin.dishes.show', $dish->id)}}">Visualizza</a>
+                    </div>
+                </div>
+            @endforeach
         </div>
-    @endforeach
+
+{{-- ---------- --}}
+
 </div>
-        <div class="align-items-center text-center">
+        <div class="d-flex justify-content-center gap-3">
             <a href="{{route('admin.dishes.create')}}" class="add-dish-button">Aggiungi Piatto</a>
             <a href="{{route('admin.orders.index')}}" class="add-dish-button">Riepilogo Ordini</a>                          
         </div>
