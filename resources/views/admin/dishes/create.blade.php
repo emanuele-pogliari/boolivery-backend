6812951d054d2de6c53a213a7ba8308d48c6d1b4
christiansaccani <christian.saccani99@gmail.com>
2024-05-29 14:47:17 +0200

    creating form donw

--- resources/views/admin/dishes/create.blade.php
+++ resources/views/admin/dishes/create.blade.php
@@ -6,12 +6,12 @@
 
     <h1>Add a dish</h1>
 
-    <form action="{{route('admin.dishes.store')}}" method="POST" enctype="multipart/form-data">
+    <form action="{{route('admin.dishes.store')}}" method="POST" enctype="multipart/form-data" class="my_form">
         @csrf
           
         <div class="mb-3 form-floating">
             <input type="text" class="form-control @error('name') is-invalid @enderror" id="name" name="name" value="{{old('name')}}" required title="Must insert dish name">
-          <label for="name" class="form-label">Dish Name</label>
+          <label for="name" class="form-label">Name</label>
           @error('image')
             <div class="invalid-feedback">
                 {{$message}}
@@ -20,7 +20,7 @@
         </div>
 
         <div class="mb-3">
-            <label for="image" class="form-label">Dish Image</label>
+            <label for="image" class="form-label"></label>
             <input type="file" class="form-control @error('image') is-invalid @enderror" id="image" name="image" aria-describedby="imgHelper" value="{{old('image')}}">
             @error('image')
             <div class="invalid-feedback">
@@ -31,7 +31,7 @@
         </div>
 
         <div class="mb-3 form-floating">
-            <textarea type="text" class="form-control @error('description') is-invalid @enderror" id="description" name="description" aria-describedby="descriptionHelper" >{{ old('description') }}</textarea>
+            <textarea style="height: 150px;" type="text" class="form-control @error('description') is-invalid @enderror" id="description" name="description" aria-describedby="descriptionHelper" >{{ old('description') }}</textarea>
             <label for="description" class="form-label">Description</label>
             @error('description')
             <div class="invalid-feedback">
@@ -54,8 +54,8 @@
         </div>
         
         <div class="mb-3 form-floating ">
-            <label for="price" class="form-label">Price</label>
             <input type="text" class="form-control @error('price') is-invalid @enderror" id="price" name="price" aria-describedby="priceHelper" value="{{old('price')}}" required title="Must insert a price">
+            <label for="price" class="form-label">Price</label>
             @error('price')
             <div class="invalid-feedback">
                 {{$message}}
@@ -64,8 +64,8 @@
             <small id="descriptionHelper" class="text-muted">Insert price between 0 and 3000</small>
         </div>
         
-        <div class="mb-3 form-check">
-            <input class="form-check-input" type="checkbox" value="1" id="visible" name="visible" aria-describedby="visibleHelper" checked>
+        <div class="mb-3 form-check my_visible">
+            <input class="form-check-input my_check" type="checkbox" value="1" id="visible" name="visible" aria-describedby="visibleHelper" checked>
             <label class="form-check-label" for="visible">
                 Visibile?
             </label>
@@ -87,7 +87,7 @@
         </div>
         @endif
 
-        <button type="submit" class="btn btn-warning">Save</button>
+        <button type="submit" class="btn my_form_btn">Save</button>
 
     </form>
 </div>
