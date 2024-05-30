3cb36efe3ba297fe41dd8dbca9d51b3e79cb1bcf
christiandigiandomenico <christian.digiandomenico1990@gmail.com>
2024-05-30 10:14:54 +0200

    Feat-Added statistics

--- app/Http/Controllers/StatsController.php
+++ app/Http/Controllers/StatsController.php
@@ -2,12 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Order;
+use App\Models\Restaurant;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class StatsController extends Controller
 {
     public function index()
     {
-        return view('admin.statistics.index');
+        $userId = Auth::id();
+        $restaurant = Restaurant::where('user_id', $userId)->firstOrFail();
+
+        $orders = Order::whereHas('dishes', function ($query) use ($restaurant) {
+            $query->where('restaurant_id', $restaurant->id);
+        })->get();
+
+        $monthlyOrders = $orders->groupBy(function ($date) {
+            return Carbon::parse($date->created_at)->format('m');
+        });
+
+        $ordersPerMonth = array_fill(0, 5, 0);
+        foreach ($monthlyOrders as $month => $orders) {
+            $ordersPerMonth[(int)$month - 1] = count($orders);
+        }
+
+        return view('admin.statistics.index', compact('ordersPerMonth'));
     }
 }
--- resources/views/admin/statistics/index.blade.php
+++ resources/views/admin/statistics/index.blade.php
@@ -1,6 +1,42 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Statistica degli Ordini</h1>
+
+<div class="container">
     
+    <div>
+        <canvas id="myChart"></canvas>
+    </div>
+
+    <a href="{{ route('admin.orders.index') }}" class="btn btn-primary">Torna agli Ordini</a>
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
+<script>
+    const ctx = document.getElementById('myChart').getContext('2d');
+    
+    const ordersPerMonth = @json($ordersPerMonth);
+
+    new Chart(ctx, {
+      type: 'bar',
+      data: {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+        datasets: [{
+          label: '# of Orders',
+          data: ordersPerMonth,
+          borderWidth: 1,
+          backgroundColor: 'rgba(54, 162, 235, 0.2)',
+          borderColor: 'rgba(54, 162, 235, 1)',
+        }]
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    });
+</script>
 @endsection
